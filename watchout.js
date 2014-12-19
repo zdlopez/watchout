b2a1717a77d10fa32bed191cb82fcb7256d4d07f
zdlopez <zachary.d.lopez@gmail.com>
2014-12-19 15:36:58 -0800

    Made circles appear- woohoo

--- watchout.js
+++ watchout.js
@@ -1,2 +1,18 @@
 // start slingin' some d3 here.
+var data = [1,2,3,4];
+
+var svg = d3.select('#gameArea').append('svg')
+  .attr('width', 1500)
+  .attr("height", 1500);
+
+
+svg.selectAll('circle')
+  .data(data)
+  .enter()
+  .append('circle')
+  .attr('r', 40 + 'px')
+  .style('fill', 'red')
+  .style('stroke', 'gray')
+  .attr('cx', function(d,i){ return d*100 })
+  .attr('cy', 100);
 
