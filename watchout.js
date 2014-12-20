c885e743a85d31dfcaa33e40d7c7f24c25915b03
zdlopez <zachary.d.lopez@gmail.com>
2014-12-19 19:53:23 -0800

    initial collision detection activated

--- watchout.js
+++ watchout.js
@@ -1,18 +1,27 @@
 // start slingin' some d3 here.
 var data = [[50, 150],[1000, 1200],[100, 800]];
+var playerData = [[400,400]];
+
 
 var svg = d3.select('#gameArea').append('svg')
   .attr('width', 800)
   .attr("height", 800);
 
+
 var drag = d3.behavior.drag().on('drag', function(d){
+  //console.log(d);
   d3.select(this)
+    // .data([d[0], d[1]])
     .attr('cy', d3.event.y)
-    .attr('cx', d3.event.x)
+    .attr('cx', d3.event.x);
+    playerData[0] = d3.event.x;
+    playerData[1] = d3.event.y;
+  //console.log(this.data());
+
   });
 
 svg.selectAll('#player')
-  .data([[400,400]])
+  .data(playerData)
   .enter()
   .append('circle')
   .attr('r', 40 + 'px')
@@ -21,6 +30,39 @@ svg.selectAll('#player')
   .attr('cx', function(d,i){ return d[0] })
   .attr('cy', function(d,i){ return d[1] })
   .call(drag);
+
+var collision = function(){
+  //var player = svg.select('#player');
+  // console.log(playerData);
+  // console.log(data);
+
+  // var top = playerData[1] - 40;
+  // var bottom = playerData[1] + 40;
+  // var right = playerData[0] + 40;
+  // var left = playerData[0] - 40;
+
+  // for (var enemy = 0; enemy < data.length; enemy++){
+  //   var etop =data[enemy][1] - 40;
+  //   var ebottom = data[enemy][1] + 40;
+  //   var eright = data[enemy][0] + 40;
+  //   var eleft = data[enemy][0] - 40;
+
+  var px = playerData[0];
+  var py = playerData[1];
+  var r2 = Math.pow((40*2), 2);
+
+  for(var enemy = 0; enemy < data.length; enemy++){
+    var ex = data[enemy][0];
+    var ey = data[enemy][1];
+
+    var distance = Math.pow(Math.abs(px - ex), 2) + Math.pow(Math.abs(py - ey), 2);
+    if(distance < r2){
+      console.log('collision');
+    }
+
+  }
+
+}
 
 
 var update = function(data){
@@ -55,4 +97,28 @@ function randomizer(array) {
   return array;
 }
 
-setInterval(function(){ return update(randomizer(data))}, 1500);
+setInterval(function(){
+  // collision();
+  return update(randomizer(data))
+}, 1500);
+
+setInterval(function(){
+  collision();
+}, 50);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
